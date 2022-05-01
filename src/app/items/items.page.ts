23b001b74f34d4df0e9bb257ae4ef30b5b0e81ff
Magali Bautista-Lee <magzb.99@gmail.com>
2022-05-01 18:52:10 -0400

    Creates component for Create Modal. Implements shopping list item toggling

--- src/app/items/items.page.ts
+++ src/app/items/items.page.ts
@@ -1,7 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { of } from 'rxjs';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ShoppingItem } from '../shared/food-item.interface';
 import { ItemsStore } from './items.store';
 
 @Component({
@@ -12,58 +10,21 @@ import { ItemsStore } from './items.store';
   providers: [ItemsStore],
 })
 export class ItemsPage {
-  readonly form: FormGroup;
   readonly vm$ = this.store.vm$;
 
-  constructor(
-    private readonly formBuilder: FormBuilder,
-    private readonly store: ItemsStore
-  ) {
-    this.form = this.formBuilder.group({
-      name: ['', Validators.required],
-      brand: [''],
-      price: [0],
-      favourited: false,
-      image: [''],
-      isInList: false,
-      notes: [''],
-      store: [''],
-    });
-  }
+  constructor(private readonly store: ItemsStore) {}
 
   delete(itemId: string): void {}
 
-  addToShoppingList(itemId: string): void {}
+  addToShoppingList(itemId: string): void {
+    this.store.toggleItemToShoppingList(itemId);
+  }
 
-  addNewItem(): void {
-    console.log('add new item');
+  handleCreatedItem(newItem: ShoppingItem): void {
+    this.store.addNewItem(newItem);
   }
 
   viewDetails(itemId: string): void {
     this.store.viewItemDetails(itemId);
-  }
-
-  cancel(): void {
-    this.resetValues();
-  }
-
-  createItem(): void {
-    console.log('createItem');
-    console.log('form values: ', this.form.value);
-    this.store.addItem(this.form.value);
-    this.resetValues();
-  }
-
-  resetValues(): void {
-    this.form.patchValue({
-      name: '',
-      brand: '',
-      price: 0,
-      favourited: false,
-      image: '',
-      isInList: false,
-      notes: '',
-      store: '',
-    });
   }
 }
